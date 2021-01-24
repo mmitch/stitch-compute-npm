6f2e69d7a0b2712ba706350312d67aba5dc7f2b6
Christian Garbs <mitch@cgarbs.de>
2021-01-24 20:48:11 +0100

    use prettier for source code formatting

--- src/stitch-compute.ts
+++ src/stitch-compute.ts
@@ -1,32 +1,30 @@
-import { NumberFormatter } from "./number-formatter"
+import { NumberFormatter } from './number-formatter';
 
 export class StitchCompute {
-    private keepFormatter = new NumberFormatter('K%d');
+  private keepFormatter = new NumberFormatter('K%d');
 
-    adjust(from: number, to: number): string {
-
-        if (from === to) {
-            return this.keep(from);
-        }
-
-        const max = 2*from;
-        const min = Math.floor((from + 1) / 2);
-        if (to > max) {
-            throw new Error(`too many stitches to add - ${from} can grow to ${max} max`)
-        }
-        if (to < min) {
-            throw new Error(`too few stitches to keep - ${from} can shrink to ${min} min`)
-        }
-
-        return '';
+  adjust(from: number, to: number): string {
+    if (from === to) {
+      return this.keep(from);
     }
 
-    setFormatters(keep: NumberFormatter): void {
-        this.keepFormatter = keep;
+    const max = 2 * from;
+    const min = Math.floor((from + 1) / 2);
+    if (to > max) {
+      throw new Error(`too many stitches to add - ${from} can grow to ${max} max`);
+    }
+    if (to < min) {
+      throw new Error(`too few stitches to keep - ${from} can shrink to ${min} min`);
     }
 
-    private keep(count: number): string {
-        return this.keepFormatter.format(count);
-    }
+    return '';
+  }
 
+  setFormatters(keep: NumberFormatter): void {
+    this.keepFormatter = keep;
+  }
+
+  private keep(count: number): string {
+    return this.keepFormatter.format(count);
+  }
 }
--- test/stitch-compute.test.ts
+++ test/stitch-compute.test.ts
@@ -2,31 +2,34 @@ import { StitchCompute } from '../src/stitch-compute';
 import { expect } from 'chai';
 import { NumberFormatter } from '../src/number-formatter';
 
-describe('StitchCompute', () => { 
-
-    describe('same number of stitches', () => {
-        it('should return K1 when staying at 1', () => {
-            expect(new StitchCompute().adjust(1, 1)).to.equal('K1');
-        });
-        it('should return K50 when staying at 50', () => {
-            expect(new StitchCompute().adjust(50, 50)).to.equal('K50');
-        });
+describe('StitchCompute', () => {
+  describe('same number of stitches', () => {
+    it('should return K1 when staying at 1', () => {
+      expect(new StitchCompute().adjust(1, 1)).to.equal('K1');
     });
+    it('should return K50 when staying at 50', () => {
+      expect(new StitchCompute().adjust(50, 50)).to.equal('K50');
+    });
+  });
 
-    describe('sanity checks', () => {
-        it('should not allow more than double the original stitches', () => {
-            expect(() => { new StitchCompute().adjust(20, 41)}).to.throw(Error, /too many.*20 can grow to 40 max/);
-        });
-        it('should not allow fewer than half the original stitches', () => {
-            expect(() => { new StitchCompute().adjust(29, 14)}).to.throw(Error, /too few.*29 can shrink to 15 min/);
-        });
-    })
+  describe('sanity checks', () => {
+    it('should not allow more than double the original stitches', () => {
+      expect(() => {
+        new StitchCompute().adjust(20, 41);
+      }).to.throw(Error, /too many.*20 can grow to 40 max/);
+    });
+    it('should not allow fewer than half the original stitches', () => {
+      expect(() => {
+        new StitchCompute().adjust(29, 14);
+      }).to.throw(Error, /too few.*29 can shrink to 15 min/);
+    });
+  });
 
-    describe('internationalization', () => {
-        it('should use alternative format strings', () => {
-            const computer = new StitchCompute();
-            computer.setFormatters(new NumberFormatter('[keep %d]'));
-            expect(computer.adjust(12, 12)).to.equal('[keep 12]');
-        })
-    })
-})
+  describe('internationalization', () => {
+    it('should use alternative format strings', () => {
+      const computer = new StitchCompute();
+      computer.setFormatters(new NumberFormatter('[keep %d]'));
+      expect(computer.adjust(12, 12)).to.equal('[keep 12]');
+    });
+  });
+});
