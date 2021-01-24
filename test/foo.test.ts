d46f614df8e6bec37f95b087934cd3bfb38d124b
Christian Garbs <mitch@cgarbs.de>
2021-01-24 20:04:37 +0100

    remove placeholder class

--- test/foo.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { Foo } from '../src/foo';
-
-describe('Foo demo test', () => {
-    it('should return foo', () => {
-        const result = Foo();
-        expect(result).to.equal('foo');
-    })
-})
